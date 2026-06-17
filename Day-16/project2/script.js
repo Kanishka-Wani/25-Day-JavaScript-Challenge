4f06e7936fd8656d58a5fa5158901337c6e4d5e3
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-06-17 13:28:25 +0000

    Project 2 solution

--- Day-16/project2/script.js
+++ Day-16/project2/script.js
@@ -1,46 +1,3 @@
-# Projects related to DOM
-
-## project link
-[Click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution code
-
-## project 1
-
-```javascript
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
-
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'purple') {
-      body.style.backgroundColor = e.target.id;
-    }
-  });
-});
-
-```
-## project 2
-
-Assignment solution
-
-```javascript
-
 const form = document.querySelector('form');
 
 //this use case will give us empty value
@@ -70,4 +27,3 @@ form.addEventListener('submit', function (e) {
   }
 });
 
-```
